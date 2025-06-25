d87d113f9769cc44961beec60708fbb9159a0827
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-06-25 11:29:07 -0300

    Conditional statements

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <TheHeader />
+  <TheHeader v-show="showHeader" />
+
+  <div v-show="showName">
+    Nome: {{ firstName }} <br>
+    Sobrenome: {{ lastName }}
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Usuario: Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Usuario: Marketing</div>
+  <div v-else>Usuario Normal</div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
@@ -13,6 +23,16 @@ export default {
   components: {
     HelloWorld,
     TheHeader
+  },
+  // Variaveis reativas
+  data() {
+    return {
+      showHeader: true,
+      firstName: "Jon",
+      lastName: "Snow",
+      showName: true,
+      accessLevel: "admin"
+    }
   }
 }
 </script>
